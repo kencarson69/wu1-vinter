d9d98a0cc2b0bd94b93976daf21367e1b1513918
kencarson69 <max.alexandersson@elev.ga.ntig.se>
2025-05-21 14:12:13 +0200

    winter wonderland

--- js/Particle.js
+++ js/Particle.js
@@ -2,10 +2,10 @@ class Particle {
   constructor(x, y, color) {
     this.x = x;
     this.y = y;
-    this.dy = (1 + Math.random() * 3) * speed / 5;
-    this.dx = (-1 + Math.random() * 2) * speed / 5;
-    this.color = `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${Math.random()})`;
-    this.size = 2 + Math.floor(Math.random() * 2);
+    this.dy = (1 + Math.random() * 10) * speed / 3;
+    this.dx = (-5 + Math.random() * 10) * speed / 2;
+    this.color = `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${Math.random(10)})`;
+    this.size = 5 + Math.floor(Math.random() * 2);
     this.toDelete = false;
   }
 
